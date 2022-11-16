e9749ecabc47fe2976fbe6d1c80aba97cd442670
Yuri Cavalini <yuricavalini@gmail.com>
2022-11-16 06:33:51 -0300

    feat(_app): add app header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
 import { globalStyles } from '../styles/global'
+
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app';
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="Logo ignite" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
