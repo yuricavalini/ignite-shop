7ee225da96a9a6a8795e7f564006e143b9a30c60
Yuri Cavalini <yuricavalini@gmail.com>
2022-11-16 05:48:55 -0300

    feat(ignite-shop): configure @stitches/react

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
+import { styled } from '../styles'
+
+export const Button = styled('button', {
+  backgroundColor: '$rockeseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)',
+  }
+})
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Button>Enviar</Button>
   )
 }
