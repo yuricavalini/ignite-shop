3a58a909d002120471d44cdd9e73081eacbab1f8
Yuri Cavalini <yuricavalini@gmail.com>
2022-11-17 08:22:32 -0300

    feat(product-page): add initial structure for product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,26 @@
 import { useRouter } from "next/router"
 
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
+
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat molestias accusantium quidem ratione enim, sapiente deserunt similique ea perferendis vel quisquam impedit natus sequi omnis consequatur animi quod incidunt iusto.</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
