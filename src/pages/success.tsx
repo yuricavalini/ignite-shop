bc5e60be31f6b1f0a463b02f50c40c64c5dd0232
Yuri Cavalini <yuricavalini@gmail.com>
2022-11-18 06:49:21 -0300

    feat(success-page): add initial structure and styles

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,23 @@
+import Link from "next/link";
+
+import { SuccessContainer, ImageContainer } from "../styles/pages/success";
+
 export default function Success() {
   return (
-    <div>Success</div>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuuul <strong>Yuri Cavalini</strong>, sua <strong>Camisa Beyound the Limits</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   )
 }
